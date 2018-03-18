349750e58ddad850c5452579658ce6207f61de82
Hguimaraes <hguimaraes@poli.ufrj.br>
2018-03-18 12:32:47 -0300

    improving documentation

---
+++ src/audiolib/__init__.py
@@ -0,0 +1,5 @@
+from .features import *
+from .generator import *
+from .models import *
+from .struct import *
+from .ttsplit import *
--- src/audiolib/struct/stransform.py
+++ src/audiolib/struct/stransform.py
@@ -3,6 +3,7 @@ import librosa
 import numpy as np
 
 # @Function: to_melspectrogram
+# @Description: Convert a set of audio songs (np arrays) to melspectrograms
 def to_melspectrogram(songs, n_fft = 2048, hop_length = 512):
   # Transformation function
   melspec = lambda x: librosa.feature.melspectrogram(x, n_fft = n_fft,
